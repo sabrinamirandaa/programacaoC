b72159d8472d2f254aef74e2a8614d20e6cda059
sabrinamirandaa <verdadesecreta792@gmail.com>
2025-04-26 19:36:47 +0000

    ATUALIZADO

--- olamundo.c
+++ olamundo.c
@@ -5,8 +5,8 @@ int main() {
     printf("Jogo Super Trunfo em C\n");
     char Estado [20];
     char Codigo [3];
-    char Cidade [20];
-    int População;
+    char Cidade [50];
+    int Populacao;
     float Area;
     float PIB;
     int Pontos_Turisticos;
@@ -21,17 +21,42 @@ int main() {
     scanf("%s", &Codigo);
     printf("Seu Codigo é: %s\n", Codigo);
 
-    printf("Digite sua Cidade: ");
+    printf("Digite sua Cidade 'Ex: RJ | SP': ");
     scanf("%s", &Cidade);
     printf("Sua Cidade é: %s\n", Cidade);
 
     printf("Digite sua População: ");
-    scanf("%d", &População);
-    printf("Sua População é: %d\n", População);
+    scanf("%d", &Populacao);
+    printf("Sua População é: %d\n", Populacao);
 
     printf("Digite sua Area (em km²): ");
     scanf("%f", &Area);
     printf("Sua Area é: %.3f km²\n", Area);
+
+    printf("Carta 2\n");
+
+    printf("Digite seu Estado- 'A' a 'H': ");
+    scanf("%s", &Estado);
+    printf("Seu Estado é: %s\n", Estado);
+
+    printf("Digite seu Codigo'ex: A01, B03': ");
+    scanf("%s", &Codigo);
+    printf("Seu Codigo é: %s\n", Codigo);
+
+    printf("Digite sua Cidade'Ex: RJ | SP': ");
+    scanf("%s", &Cidade);   
+    printf("Sua Cidade é: %s\n", Cidade);
+
+    printf("Digite a População: ");
+    scanf("%d", &Populacao);
+    printf("Sua Populacao é: %d\n", Populacao);
+
+    printf("Digite sua Area: ");
+    scanf("%f", &Area);
+    printf("Sua Area é: %2.f km²\n", Area);
+
+    return 0;
+
 
 
 
